08a8d0b409c3b25fa0ff4c6ec149e831e081ae45
Пользователь <krrpacho@gmail.com>
2024-07-31 14:37:53 +0300

    Update

--- src/main/java/com/example/back/WebConfig.java
+++ src/main/java/com/example/back/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.back;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://krrpacho.github.io/produo/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("https://krrpacho.github.io")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
